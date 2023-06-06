2984d1f6f9e511c7c989bdb31e5d5923c4878661
Ashutosh Kumar Giri <93449692+ashutoshgiri182002@users.noreply.github.com>
2023-06-06 13:31:52 +0530

    Update mergeIntervals.cpp
    https://www.codingninjas.com/codestudio/problems/699917?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website

--- Array2/mergeIntervals.cpp
+++ Array2/mergeIntervals.cpp
@@ -1,3 +1,29 @@
+//Optimise Approach
+
+vector<vector<int>> mergeIntervals(vector<vector<int>> &arr)
+{
+    // Write your code here.
+    sort(arr.begin(),arr.end());
+    vector<vector<int>>ans;
+    int n = arr.size();
+
+
+    for(int i = 0;i<n;i++){
+        if(ans.empty() or ans.back()[1]<arr[i][0]){
+            ans.push_back(arr[i]);
+        }
+        else{
+            ans.back()[1] = max(ans.back()[1] , arr[i][1]);
+        }
+    }
+
+    return ans;
+    
+
+}
+
+
+//Brute force approach
 vector<vector<int>> mergeOverlappingIntervals(vector<vector<int>> &arr){
 	// Write your code here.
 	int n = arr.size();
