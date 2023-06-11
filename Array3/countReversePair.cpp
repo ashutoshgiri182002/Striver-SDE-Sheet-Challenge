9db13fc8191b40015e0eae219c49e4703a26f543
Ashutosh Kumar Giri <93449692+ashutoshgiri182002@users.noreply.github.com>
2023-06-11 13:15:40 +0530

    Create countReversePair.cpp

---
+++ Array3/countReversePair.cpp
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculator</title>
+    <link rel="stylesheet" href="Calculator.css">
+</head>
+<body>
+    <navbar class="nav">
+        <div class="left">
+            <img src="gym logo.png" alt="ERROR LOADING IMAGE">
+            <h3>ABC FITNESS</h3>
+        </div>
+        <div class="mid">
+            <ul>
+                <li><a href="Home">Home</a></li>
+                <li><a href="About">About Us</a></li>
+                <li><a href="Calculator">BMI Calculator</a></li>
+                <li><a href="Contact Us">Contact Us</a></li>
+            </ul>
+        </div>
+        <div class="right">
+            <button class="btn">Call Us</button>
+            <button class="btn">Email</button>
+        </div>
+    </navbar>
+</body>
+</html>
