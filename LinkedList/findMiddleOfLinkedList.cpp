aa3dd7ca04bd835de9c33c0945ee266e2cd2f02a
Ashutosh Kumar Giri <93449692+ashutoshgiri182002@users.noreply.github.com>
2023-06-14 10:56:58 +0530

    Create findMiddleOfLinkedList.cpp

---
+++ LinkedList/findMiddleOfLinkedList.cpp
@@ -0,0 +1,13 @@
+Node *findMiddle(Node *head) {
+    // Write your code here
+    Node*slow = head;
+    Node*fast = head;
+
+    while(fast and fast->next){
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow;
+
+}
