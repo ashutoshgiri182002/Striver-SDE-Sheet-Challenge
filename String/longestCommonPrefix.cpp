5a9c44ff82f5be7703a9e2ff436678a619610788
Ashutosh Kumar Giri <93449692+ashutoshgiri182002@users.noreply.github.com>
2023-07-27 00:12:57 +0530

    Update longestCommonPrefix.cpp

--- String/longestCommonPrefix.cpp
+++ String/longestCommonPrefix.cpp
@@ -27,3 +27,33 @@ string longestCommonPrefix(vector<string> &arr, int n)
 }
 
 
+////Another approach 
+
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        
+        int n = strs.size();
+        if(n==0) return "";
+        
+        string res  = "";
+        sort(begin(strs), end(strs));
+        string a = strs[0];
+        string b = strs[n-1];
+        
+        for(int i=0; i<a.size(); i++){
+            if(a[i]==b[i]){
+                res = res + a[i];
+            }
+            
+            else{
+                break;
+            }
+            
+        }
+        
+        return res;
+    }
+};
+
